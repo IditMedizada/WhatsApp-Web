1e0da01b3814c2f11cdcb1edc1eac21830715c87
tomerp1812 <tomerp1812@gmail.com>
2023-06-01 09:30:58 +0300

    changed the vission of the recieved messages

--- teamchat/src/Chat/LeftPanel/Contacts/ContactListResults.js
+++ teamchat/src/Chat/LeftPanel/Contacts/ContactListResults.js
@@ -14,31 +14,6 @@ function ContactListResults({ contacts, changeContact, me, sentList }) {
     changeContact(contact);
   };
 
- // sorting the contacts by the last message sent
-  // const sortedContacts = contacts.sort((contactA, contactB) => {
-  //   return contactA.lastMessage.created - contactB.lastMessage.created;
-
-  
-  //   const sortedMessages = fetchedData.sort((a, b) => {
-  //     return new Date(a.created) - new Date(b.created);
-  //   });
-
-
-  //   if(contactA.lastMessage.content === ""){
-  //     return 1
-  //   }
-  //   if(contactB.lastMessage.content === ""){
-  //     return -1;
-  //   }
-  //   if (contactA.lastMessage.created < contactB.lastMessage.created) {
-  //     return 1;
-  //   } else if (contactA.lastMessage.created > contactB.lastMessage.created) {
-  //     return -1;
-  //   } else {
-  //     return 0;
-  //   }
-  // });
-
   //make a list of all the contacts
   const contactsList = contacts.map((contact, key) => {
     const isSelected = contact === selectedContact;
